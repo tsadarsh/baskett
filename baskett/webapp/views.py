b45bc1b73b660b9265b11af0a2b7fa3c89ef4888
Adarsh TS <tsadarsh0707@gmail.com>
2022-05-28 11:52:17 +0530

    Feature: View all products

--- baskett/webapp/views.py
+++ baskett/webapp/views.py
@@ -4,12 +4,20 @@ from .models import Product, Category
 
 
 def home(request):
+	"""Home page lists the categories available to choose from"""
 	categories = Category.objects.all
 	return render(request, 'webapp/home.html', {'categories': categories})
 
 def index(request, category):
-	category_id = Category.objects.filter(category=category)[0].id
-	products = Product.objects.filter(category=category_id)
+	"""Returns products that match the category provied. If category is
+	'all' then return all the products from DB.
+	"""
+	if category == "all":
+		products = Product.objects.all()
+	else:
+		category_id = Category.objects.filter(category=category)[0].id
+		products = Product.objects.filter(category=category_id)
+
 	return render(request, 'webapp/index.html', {'products': products})
 
 def detail(request, product_id):
