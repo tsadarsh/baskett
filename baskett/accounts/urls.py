cb622a040b01771bd27a1aec9943509d69144e0f
Adarsh TS <tsadarsh0707@gmail.com>
2022-05-30 23:30:17 +0530

    Fix: Authenticate user before heading to home page

--- baskett/accounts/urls.py
+++ baskett/accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
+	path('auth/', views.auth, name="auth"),
 	path('login/', views.login, name="login"),
 ]
--- baskett/accounts/views.py
+++ baskett/accounts/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate as dj_auth
+from django.contrib.auth import login as dj_login
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def login(request):
 	return render(request, 'accounts/login.html')
+
+def auth(request):
+	username = request.POST['uname']
+	password = request.POST['passwd']
+	user = dj_auth(
+		request,
+		username=username,
+		password=password
+	)
+
+	if user is not None:
+		dj_login(request, user)
+		return HttpResponseRedirect(reverse('webapp:home'))
+
+	else:
+		error_message = "Incorrect credentials!"
+		return render(
+			request,
+			'accounts/login.html',
+			{'error_message': error_message}
+		)
