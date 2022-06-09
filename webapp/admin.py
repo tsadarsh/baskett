0a46d9e5b7127dca32840e2785a2fbeedfd3cf1b
Adarsh TS <tsadarsh0707@gmail.com>
2022-06-09 22:18:21 +0530

    [Update]: Register new model - Item

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import Seller, Product, Category
+from .models import Seller
+from .models import Product
+from .models import Category
+from .models import Item
 
 admin.site.register(Seller)
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(Item)
