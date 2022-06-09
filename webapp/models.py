734ee0fb019e1fe5f178deb8db61edfdc860e701
Adarsh TS <tsadarsh0707@gmail.com>
2022-06-09 21:48:12 +0530

    [Update]: Create new foriegn field for Product name

--- webapp/models.py
+++ webapp/models.py
@@ -5,7 +5,8 @@ class Seller(models.Model):
 	"""Each product comes under one seller
 
 	Example: Big Basket"""
-	seller_name = models.CharField(max_length=200)
+	seller_name = models.CharField(max_length=200, unique=True)
+	available = models.BooleanField(default=True)
 
 	def __str__(self):
 		return self.seller_name
@@ -20,10 +21,13 @@ class Category(models.Model):
 	def __str__(self):
 		return self.category
 
+class Item(models.Model):
+	name = models.CharField(max_length=100)
+
 class Product(models.Model):
-	"""Describes the product attributes"""
-	seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
-	product_name = models.CharField(max_length=300)
+	"""Describes the Item attributes"""
+	seller = models.ForeignKey(Seller, on_delete=models.PROTECT)
+	product_name = models.ForeignKey(Item, on_delete=models.PROTECT)
 	category = models.ForeignKey(Category, on_delete=models.CASCADE)
 	quantity = models.IntegerField(default=0)
 	rating = models.IntegerField(default=0)
@@ -33,16 +37,6 @@ class Product(models.Model):
 
 class History(models.Model):
 	"""Displays the history of oders placed by the User """
-	item = models.ForeignKey(
-		Product,
-		on_delete=models.SET_NULL,
-		blank=True,
-		null=True
-	)
-	buyer = models.ForeignKey(
-		User,
-		on_delete=models.SET_NULL,
-		blank=True,
-		null=True
-	)
+	item = models.ForeignKey(Product, on_delete=models.SET_NULL)
+	buyer = models.ForeignKey(User, on_delete=models.SET_NULL)
 	quantity = models.IntegerField(default=0)
